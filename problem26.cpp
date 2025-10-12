77d8d703d7982a37ce1760692718885d989f7500
unknown <sarikjain1234@gmail.com>
2025-10-12 20:41:46 +0530

    problem 26

---
+++ problem26.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   
+//Russian Peasent problem 
+int a;
+int b;
+int d=0;
+int c;
+int e;
+int f;
+int sum=0;
+cout<<"enter number 1: "<<endl;
+cin>>a;
+cout<<"enter number 2: "<<endl;
+cin>>b;
+f=a;
+e=b;
+while(c>1){
+c=a/2;
+a=c;
+d=b*2;
+b=d;
+if(c%2!=0){
+sum=sum+d;
+}
+
+}
+if(f%2==0){
+cout<<"Multiplictaion is: "<<endl;
+cout<<sum;}
+else if(f%2!=0){
+cout<<"Multiplictaion is: "<<endl;
+cout<<sum+e;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
