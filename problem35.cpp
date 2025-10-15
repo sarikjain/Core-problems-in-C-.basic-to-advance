a8d7d5a47acc64c9499d7c3c729062a45c621353
unknown <sarikjain1234@gmail.com>
2025-10-15 22:47:11 +0530

    problem 35

---
+++ problem35.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+//frst n numbers in base y
+int n;
+cin>>n;
+
+
+int c;
+int d;
+
+for(int i=0;i<n;i++){
+
+c=i%6;
+d=i/6;
+
+if(d>0){
+cout<<d;
+}
+
+cout<<c<<endl;
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
