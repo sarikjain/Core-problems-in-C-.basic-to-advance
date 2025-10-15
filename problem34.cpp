b6ec1e4420c7ab4fa849e1d86106abfd0ee88298
unknown <sarikjain1234@gmail.com>
2025-10-15 21:36:29 +0530

    problem 34

---
+++ problem34.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+//opposite sign
+int a;
+int b;
+cout<<"Enter number 1 : "<<endl;
+cin>>a;
+cout<<"Enter number 2 : "<<endl;
+cin>>b;
+if((a^b)<0){
+    cout<<"numbers are opposite"<<endl;
+
+}
+else{
+
+    cout<<"numbers are negative "<<endl;
+}
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
