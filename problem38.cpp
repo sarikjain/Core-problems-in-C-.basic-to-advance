30459c016c33294fb2102ac90e143e938f72b634
unknown <sarikjain1234@gmail.com>
2025-10-21 22:39:21 +0530

    problem 38

---
+++ problem38.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+//vectors
+
+
+
+
+vector <int> ver={1,2,3};
+cout<<ver.size();
+ver.push_back(9);
+ver.pop_back();
+cout<<ver.size();
+
+for(int i:ver){
+    cout<<i<<endl;
+
+}
+vector <int> ver1;
+
+ver1.push_back(1);
+ver1.push_back(2);
+cout<<ver1.size()<<endl;
+cout<<ver1.capacity();
+
+//size=capacity
+//heap for dynamic allocated memory and run time programming
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
