0eba5bfa44e2a824a85512bf7f451b96ff1fcb67
unknown <sarikjain1234@gmail.com>
2025-10-23 10:24:26 +0530

    problem 46

---
+++ problem46.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+   
+    
+
+
+string str="ABC";
+
+sort(str.begin(),str.end());
+
+do{
+    cout<<str<<endl;
+
+}
+while(next_permutation(str.begin(),str.end()));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
