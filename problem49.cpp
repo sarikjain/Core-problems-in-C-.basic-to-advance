66eac919b310fe7b2eb2e984decb153041e00df1
unknown <sarikjain1234@gmail.com>
2025-10-23 17:49:40 +0530

    problem 49

---
+++ problem49.cpp
@@ -0,0 +1,101 @@
+   
+#include<iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+
+int arr[4]={1,2,3,4};
+
+
+
+for(int st=0;st<4;st++){
+
+for(int end=st;end<4;end++){
+
+    for(int i=st;i<=end;i++){
+        cout<<arr[i];
+    }
+
+
+cout<<" ";
+
+
+
+
+}
+
+
+
+
+cout<<endl;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
