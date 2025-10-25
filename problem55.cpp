732d0380982872b83a737ba86b3c481dd340d278
unknown <sarikjain1234@gmail.com>
2025-10-25 23:39:11 +0530

    problem 55

---
+++ problem55.cpp
@@ -0,0 +1,67 @@
+   
+#include<iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+
+int arr[7]={1,2,3,4,5,6,7};
+int target ;
+cin>>target;
+
+for(int i=0;i<7;i++){
+
+
+    for(int j=i+1;j<7;j++){
+        if(arr[i]+arr[j]==target){
+            cout<<arr[i]<<","<<arr[j]<<" ";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
