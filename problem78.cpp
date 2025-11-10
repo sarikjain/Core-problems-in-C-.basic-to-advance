5b8754c754409e2fd71d19a32ea0a552642aeaab
unknown <sarikjain1234@gmail.com>
2025-11-10 13:33:10 +0530

    problem 78

---
+++ problem78.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void subsets(vector<int> &x, int n, vector<int> &v, int i)
+{
+
+    if (i == n)
+    {
+        for (int var : v)
+        {
+            cout << var << " ";
+        }
+        return;
+    }
+
+    v.push_back(v[i]);
+    subsets(x, n, v, i + 1);
+
+    v.pop_back();
+    subsets(x, n, v, i + 1);
+}
+
+int main()
+{
+
+    vector<int> v;
+    vector<int> g = {1, 2, 3, 4, 5};
+    subsets(g, 5, v, 0);
+
+    cout << "hi";
+}
