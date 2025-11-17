d54bde5aa0f0142870f3327d1078cafaf289a7da
unknown <sarikjain1234@gmail.com>
2025-11-17 22:23:10 +0530

    problem 89

---
+++ problem89.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+
+
+
+
+
+int main(){
+string s[4]={"abc","abcd","aba","abd"};
+sort(s+0,s+3);
+
+for(int i=0;i<4;i++){
+    cout<<s[i]<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+    
+}
